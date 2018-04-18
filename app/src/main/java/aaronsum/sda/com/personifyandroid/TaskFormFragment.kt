1f557a97ebe2775e9a8147650a29ff518f23c686
aaronsum1102 <sumsx03@gmail.com>
2018-04-18 07:19:29 +0200

    Implement text watcher for text input.
    - Confirm button will only be enabled when task name, status, due date
      and priority were given by user.

--- app/src/main/java/aaronsum/sda/com/personifyandroid/TaskFormFragment.kt
+++ app/src/main/java/aaronsum/sda/com/personifyandroid/TaskFormFragment.kt
@@ -1,18 +1,22 @@
 package aaronsum.sda.com.personifyandroid
 
+import android.app.DatePickerDialog
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_task_form.*
+import java.util.*
 
-class TaskFormFragment : Fragment() {
+class TaskFormFragment : Fragment(), TextWatcher {
     private var existingTask: Task? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -22,7 +26,14 @@ class TaskFormFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        addTaskButton.isEnabled = false
         removeTaskButton.visibility = View.INVISIBLE
+
+        taskNameText.addTextChangedListener(this)
+        dueDate.addTextChangedListener(this)
+        status.addTextChangedListener(this)
+        priority.addTextChangedListener(this)
+
         val viewModel = ViewModelProviders.of(activity!!)[TaskViewModel::class.java]
         val taskId = arguments?.getInt(TaskListFragment.KEY_TASK_ID) ?: 0
         if (taskId != 0) {
@@ -81,6 +92,29 @@ class TaskFormFragment : Fragment() {
                             })
         }
 
-
+        dueDate.setOnClickListener {
+            val calender = Calendar.getInstance()
+            val datePickerDialog = DatePickerDialog(context,
+                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                        dueDate.setText("$year-${month + 1}-$dayOfMonth")
+                    },
+                    calender[Calendar.YEAR],
+                    calender[Calendar.MONTH],
+                    calender[Calendar.DAY_OF_MONTH])
+            datePickerDialog.show()
+        }
     }
+
+
+    override fun afterTextChanged(s: Editable?) {
+        addTaskButton.isEnabled = taskNameText.text.isNotEmpty() &&
+                dueDate.text.isNotEmpty() &&
+                status.text.isNotEmpty() &&
+                priority.text.isNotEmpty()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 }
+
