cac665b70a5e3540ad1f449ec215f358d2e95df3
Aaron Sum <sumsx03@gmail.com>
2018-05-16 08:30:09 +0200

    Adjust task form layout for better user experience.

--- app/src/main/java/aaronsum/sda/com/personifyandroid/MainActivity.kt
+++ app/src/main/java/aaronsum/sda/com/personifyandroid/MainActivity.kt
@@ -2,6 +2,8 @@ package aaronsum.sda.com.personifyandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +12,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.container, UserManagementFragment())
-                    .commit()
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            auth.signInAnonymously().addOnCompleteListener {
+                Toast.makeText(this,
+                        "Welcome back, ${currentUser.displayName}",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container, TaskListFragment())
+                        .commit()
+            }
+        } else {
+            if (savedInstanceState == null) {
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container, UserManagementFragment())
+                        .commit()
+            }
         }
     }
 }
