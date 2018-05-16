8351a9f6feb50ace5b004649e706715e3db70989
Aaron Sum <sumsx03@gmail.com>
2018-05-17 01:27:29 +0200

    Add email address validity check to enable confirm button for password reset email

--- app/src/main/java/aaronsum/sda/com/personifyandroid/ResetPasswordFragment.kt
+++ app/src/main/java/aaronsum/sda/com/personifyandroid/ResetPasswordFragment.kt
@@ -3,6 +3,8 @@ package aaronsum.sda.com.personifyandroid
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +19,21 @@ class ResetPasswordFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val userViewModel = ViewModelProviders.of(activity!!)[UserViewModel::class.java]
+
+        confirmButton.isEnabled = false
+
+        emailText.addTextChangedListener (
+            object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    confirmButton.isEnabled = emailText.text.toString().contains("@") &&
+                            emailText.text.toString().contains(".com")
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            }
+        )
 
         confirmButton.setOnClickListener {
             val email = emailText.text.toString()
