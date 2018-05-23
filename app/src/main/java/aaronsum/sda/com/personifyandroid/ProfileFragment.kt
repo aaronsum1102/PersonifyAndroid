a1217d605c05ebfd2e032c87881c074abb5965a5
Aaron Sum <sumsx03@gmail.com>
2018-05-23 15:13:30 +0200

    Add log out process for user.

--- app/src/main/java/aaronsum/sda/com/personifyandroid/ProfileFragment.kt
+++ app/src/main/java/aaronsum/sda/com/personifyandroid/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package aaronsum.sda.com.personifyandroid
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -8,6 +10,8 @@ import android.widget.PopupMenu
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
+    private lateinit var userViewModel: UserViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
@@ -15,7 +19,10 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initialisedToolbar("aaron")
+        userViewModel = ViewModelProviders.of(activity!!)[UserViewModel::class.java]
+        userViewModel.currentUser.observe(this, Observer { user ->
+            user?.let { initialisedToolbar(it.username) }
+        })
     }
 
     private fun initialisedToolbar(username: String) {
@@ -38,11 +45,31 @@ class ProfileFragment : Fragment() {
                 R.id.action_settings -> {
                     val anchor = activity?.findViewById<View>(R.id.action_settings)
                     val menu = PopupMenu(context, anchor)
+                    menu.setOnMenuItemClickListener { settingMenuSelector(it) }
                     menu.inflate(R.menu.profile_setting_menu)
                     menu.show()
                 }
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun settingMenuSelector(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.actionEditProfile -> {
+
+            }
+
+            R.id.actionLogOut -> {
+                userViewModel.signOut()
+                fragmentManager?.apply {
+                    popBackStack()
+                    beginTransaction()
+                            .replace(R.id.container, UserManagementFragment())
+                            .commit()
+                }
+            }
+        }
+        return true
     }
 }
